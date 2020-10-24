ecc8f6f6e97da229807666bbc914735bcf841df3
Auron0330 <auron0330@gmail.com>
2020-10-24 23:20:05 +0900

    add line_bot controller

---
+++ app/controllers/line_bot_controller.rb
@@ -0,0 +1,39 @@
+class LineBotController < ApplicationController
+
+  protect_from_forgery except: [:callback]
+
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+        end
+      end
+      client.reply_message(event['replyToken'], message)
+    end
+    head :ok
+  end
+
+  private
+
+  # LINE Developers登録完了後に作成される環境変数の認証
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+end
